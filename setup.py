860f27c14363e1882f6769ab8fa721156d8a2741
India Marsden <imm1117@ic.ac.uk>
2024-07-04 11:32:02 +0100

    add setup.py, change gitignore

--- setup.py
+++ setup.py
@@ -1,3 +1,10 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
-setup(name='redefining_fe', version='0.1.0', packages=['redefining_fe'])
+
+setup(name='redefining_fe',
+      version='0.1.0',
+      description="A language to completely specify the finite element",
+      author="India Marsden",
+      author_email="marsden@maths.ox.ac.uk",
+
+      packages=find_packages())
