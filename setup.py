1961c4c81164d1d7e35f6b8127514bba0441321f
India Marsden <imm1117@ic.ac.uk>
2024-11-26 10:19:49 +0000

    change to pyproject.toml

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-
-
-setup(name='redefining_fe',
-      version='0.1.0',
-      description="A language to completely specify the finite element",
-      author="India Marsden",
-      author_email="marsden@maths.ox.ac.uk",
-      packages=find_packages())
